9b9c7499619de2d7c75115e7cbb1fe63abf54f9c
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2024-09-26 23:40:42 +0530

    Time: 4 ms (76.6%), Space: 26 MB (77.3%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        int x = 1;
         int n = nums.size();
         sort(nums.begin(),nums.end());
         if(nums[n-1]!=n-1 || nums[n-2]!=n-1){
             return false;
         }
         for(int i=0;i<n-2;i++){
-            if(nums[i]==x){
-                x++;
+            if(nums[i]==i+1){
+                continue;
             }
             else{
                 return false;
