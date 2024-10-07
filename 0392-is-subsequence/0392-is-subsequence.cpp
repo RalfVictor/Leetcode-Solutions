3670f8a811fc51b5067c0e951b76e1fe55757329
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2024-10-07 23:19:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        vector<char> sub;
+        vector<char> sen;
+        int n = s.length();
+        int m = t.length();
+        if(m<n){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            sub.push_back(s[i]);
+        }
+        for(int i=0;i<m;i++){
+            sen.push_back(t[i]);
+        }
+        auto it = sen.begin();
+        for(int i=0;i<n;i++){
+            auto x = find(sen.begin(),sen.end(),sub[i]);
+            if(count(sen.begin(),sen.end(),sub[i])==0 || x<it){
+                return false;
+            }
+            if(count(sen.begin(),sen.end(),sub[i])>0){
+                sen.erase(sen.begin(),x);
+                cout<<*x;
+                it = (x+1);
+            }
+        }return true;
+    }
+};
