6cf71ac2269e965523134327bf8a03382d4bcba1
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2025-02-25 17:24:37 +0530

    Time: 7 ms (0.92%) | Memory: 9.5 MB (89.43%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        int count = 0;
+        int col = 1;
+        for(int i=1;i<=numRows;i++){
+            int res = 1;
+            for(int j = 0;j<col;j++){
+                if(j == 0){
+                    temp.push_back(1);
+                    cout<<j<<" ";
+                    continue;
+                }
+                else{
+                    cout<<j<<" ";
+                    res = res*(col-j);
+                    res = res/j;
+                    temp.push_back(res);
+                } 
+            }
+        ans.push_back(temp);
+        temp.clear();
+        col++;
+        }
+        return ans;
+    }
+};
