8ddbf89b490647e9202b99badb636e50fec498a3
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2025-03-07 17:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (51.89%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,60 +9,64 @@
  */
 class Solution {
 public:
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int> ans;
-        unordered_map<int, TreeNode*> parent;
-        queue<TreeNode*> q;
+    void markparents(TreeNode* root , unordered_map<TreeNode*, TreeNode*>&parent_tracker){
+        queue<TreeNode*>q;
         q.push(root);
-
         while(!q.empty()){
-            int si = q.size();
-            for(int i = 0; i < si; i++){
-                auto top = q.front();
-                q.pop();
-                
-                if(top -> left){
-                    parent[top->left->val] = top;
-                    q.push(top->left); 
-                }
-
-                if(top -> right){
-                    parent[top->right->val] = top;
-                    q.push(top->right); 
-                }
-            }
-        }
-
-        unordered_map<int, int> visited;
-        q.push(target);
-        while(k-- && !q.empty()){
-            int size = q.size();
-
-            for(int i = 0; i < size; i++){
-                auto top = q.front();
-                q.pop();
-
-                visited[top -> val] = 1;
-
-                if(top -> left && !visited[top->left->val]){
-                    q.push(top -> left);
-                }
-
-                if(top -> right && !visited[top->right->val]){
-                    q.push(top -> right);
-                }
-
-                if(parent[top->val] && !visited[parent[top->val] -> val]){
-                    q.push(parent[top->val]);
-                }
-
-            }
-        }
-
-        while(!q.empty()){
-            ans.push_back(q.front()->val);
+            TreeNode* node= q.front();
             q.pop();
+            if(node->left){
+                parent_tracker[node->left]=node;
+                q.push(node->left);
+            }
+            if(node->right){
+                parent_tracker[node->right]=node;
+                q.push(node->right);
+            }
+        }
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int>ans;
+        if(target==NULL) return {};
+        unordered_map<TreeNode*, TreeNode*>parent_tracker;
+        markparents(root,parent_tracker);
+        unordered_map<TreeNode* , bool>visited;
+        queue<TreeNode*>q;
+        q.push(target);
+        visited[target]=true;
+        int dis=0;
+        while(!q.empty()){
+            int siz= q.size();
+            if(dis==k){
+                break;
+            }
+            dis++;
+            for(int i=0;i<siz;i++){
+                TreeNode* current= q.front();
+                q.pop();
+                if(current->left && !visited[current->left]){
+                    q.push(current->left);
+                    visited[current->left]=true;
+                }
+                if(current->right && !visited[current->right]){
+                    q.push(current->right);
+                    visited[current->right]=true;
+                }
+                if(parent_tracker[current] && !visited[parent_tracker[current]]){
+                    q.push(parent_tracker[current]);
+                    visited[parent_tracker[current]]=true;
+                }
+
+            }
+
+        }
+        
+        while(!q.empty()){
+            TreeNode* curr=q.front();
+            q.pop();
+            ans.push_back(curr->val);
         }
         return ans;
+        
     }
 };
