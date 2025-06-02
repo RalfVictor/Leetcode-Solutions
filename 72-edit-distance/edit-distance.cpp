f6bb955f71ed0a21b1f96f75cc4abb28ea44738b
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2025-06-02 12:59:53 +0530

    Time: 4 ms (81.09%) | Memory: 13.2 MB (32.57%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -18,6 +18,17 @@ public:
         int n = text1.length();
         int m = text2.length();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,text1,text2,dp);
+        for(int i=0;i<=n;i++) dp[i][0] = i;
+        for(int i=0;i<=m;i++) dp[0][i] = i;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+            if(text1[i-1]==text2[j-1]){
+            dp[i][j] = dp[i-1][j-1];
+        }else{
+            dp[i][j] = 1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+        }
+        }
+        }
+        return dp[n][m];
     }
 };
