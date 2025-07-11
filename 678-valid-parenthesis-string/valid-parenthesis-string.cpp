55fc391da78a081c31139826625656f23cb4c638
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2025-07-11 23:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (92.53%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -17,8 +17,28 @@ public:
 
     bool checkValidString(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        /*vector<vector<int>> dp(n,vector<int>(n,-1));
         if(n==1) return s[0]=='*';
         return check(s,0,0,n,dp);
+        */
+        int min = 0;
+        int max = 0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                min++;
+                max++;
+            }
+            else if(s[i]==')'){
+                min--;
+                max--;
+            }
+            else{
+                min--;
+                max++;
+            }
+            if(min<0) min = 0;
+            if(max<0) return false;
+        }
+        return min==0;
     }
 };
