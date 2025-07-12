58860e467aab24febd8904036cce88874d8891ab
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2025-07-12 19:24:37 +0530

    Time: 438 ms (12.44%) | Memory: 52.4 MB (33.15%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return true;
+        if(nums[0]==0) return false;
+        int maxi = 0;
+        for(int i=0;i<n-1;i++){
+            if(i>maxi) return false;
+            maxi = max(maxi,i+nums[i]);
+            cout<<maxi<<" ";
+            if(maxi>=n-1) return true;
+        }
+        return false;
+    }
+};
