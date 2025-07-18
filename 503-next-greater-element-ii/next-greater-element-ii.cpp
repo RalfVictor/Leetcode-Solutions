ce1471feceedfd4230f3ea97debfcf671ff98d54
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2025-07-18 18:28:06 +0530

    Time: 91 ms (7.82%) | Memory: 28 MB (94.88%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> x;
-        vector<int> ans;
+        vector<int> ans(n,-1);
         for(int i=0;i<n;i++){
             int flag = 0;
             for(int j=1;j<n;j++){
                 if(nums[i]<nums[(j+i)%n]){
-                    x[i] = nums[(j+i)%n];
-                    if(x[i]==0) flag = 1;
+                    ans[i] = nums[(j+i)%n];
                     break;
                 }
             }
-            if(flag == 1) ans.push_back(x[i]);
-            else if(x[i]==0) ans.push_back(-1);
-            else ans.push_back(x[i]);
         }
         return ans;
     }
