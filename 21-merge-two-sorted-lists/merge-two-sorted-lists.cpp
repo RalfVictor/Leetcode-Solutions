b07d94d6fbb14529f7e06aa2bf114c152ead23fb
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2025-10-09 10:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (28.90%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,42 +11,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL && list2==NULL){
-            return NULL;
-        }
-        if(list1==NULL){
-            return list2;
-        }
-        else if(list2==NULL){
-            return list1;
-        }
-        ListNode* dummy = new ListNode(-1);
-        ListNode* temp = dummy;
-        while(list1!=NULL && list2!=NULL){
-            int a = list1->val;
-            int b = list2->val;
-            if(a<b){
-                temp->next = list1;
-                list1 = list1->next;
-                temp = temp->next;
-            }
-            else if(a>b){
-                temp->next = list2;
+        ListNode* head = new ListNode(-1);
+        ListNode* start = head;
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val >= list2->val){
+                ListNode* x = new ListNode(list2->val);
+                head->next = x;
+                head = head->next;
                 list2 = list2->next;
-                temp = temp->next;
             }
             else{
-                temp->next = list1;
+                ListNode* x = new ListNode(list1->val);
+                head->next = x;
+                head = head->next;
                 list1 = list1->next;
-                temp = temp->next;
             }
         }
-        if(list1 == NULL){
-            temp->next = list2;
+        if(list2 == NULL){
+            while(list1){
+                ListNode* x = new ListNode(list1->val);
+                head->next = x;
+                head = head->next;
+                list1 = list1->next;
+            }
         }
-        else if(list2 == NULL){
-            temp->next = list1;
+        else{
+            while(list2){
+                ListNode* x = new ListNode(list2->val);
+                head->next = x;
+                head = head->next;
+                list2 = list2->next;
+             }
         }
-        return dummy->next;
+        return start->next;
     }
 };
