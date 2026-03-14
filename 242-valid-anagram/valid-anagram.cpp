4131ce64ce040325576aaa6a3235ce57f3575192
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2026-03-14 10:23:41 +0530

    Time: 1 ms (55.22%) | Memory: 9.9 MB (23.60%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    int n = s.size();
+    int m = t.size();
+    if(n!=m) return false;
+    unordered_map<char,int> mp;
+    for(int i=0;i<n;i++){
+        mp[s[i]]++;
+        mp[t[i]]--;
+    }
+    for(auto it:mp){
+        if(it.second!=0){
+            return false;
+        }
+    }
+    return true;
+    }
+};
