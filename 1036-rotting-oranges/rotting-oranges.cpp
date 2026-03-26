9da392c818d3f04e0a390037edc42f4da6c88edf
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2026-03-26 10:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (53.62%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,48 +3,44 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        int nco = 0;
         queue<pair<pair<int,int>,int>> q;
-        int count = 0;
-        int ncount = 0;
-        int time = 0;
         int vis[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    vis[i][j]=2;
                     q.push({{i,j},0});
-                   
+                    vis[i][j] = 1;
                 }
                 else if(grid[i][j]==1){
-                    vis[i][j]=0;
-                    ncount++;
+                    nco++;
+                    vis[i][j] = 0;
                 }
                 else{
-                    vis[i][j]=0;
+                    vis[i][j] = 0;
                 }
             }
         }
-        int r[] = {-1,0,1,0};
-        int c[] = {0,1,0,-1};
+        int co = 0;
+        int nr[] = {-1,0,1,0};
+        int nc[] = {0,-1,0,1};
+        int t = 0;
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int t = q.front().second;
-            time = max(t,time);
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            t = q.front().second;
             q.pop();
             for(int i=0;i<4;i++){
-                int nr = row+r[i];
-                int nc = col+c[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && vis[nr][nc]==0){
-                    q.push({{nr,nc},t+1});
-                    vis[nr][nc]=2;
-                    count++;
+                int row = r+nr[i];
+                int col = c+nc[i];
+                if(row>=0 && row<n && col>=0 && col<m && grid[row][col]==1 && vis[row][col]==0){
+                    vis[row][col] = 1;
+                    q.push({{row,col},t+1});
+                    co++;
                 }
             }
         }
-        if(ncount!=count){
-            return -1;
-        }
-        return time;
+        if(nco!=co) return -1;
+        return t;
     }
 };
