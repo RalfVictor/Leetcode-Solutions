1aebff89f6bfb9e5a5c91f11a2b3c7f962401aa8
Ralf_Victor <142431936+RalfVictor@users.noreply.github.com>
2026-03-26 10:32:18 +0530

    Time: 8 ms (35.77%) | Memory: 20.3 MB (19.02%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& pre) {
-       vector<int> ans;
-        vector<vector<int>> adj(numCourses);
-        for(auto it:pre){
-            adj[it[1]].push_back(it[0]);
-        }
-        vector<int> ind(numCourses, 0);
-        for(int i=0;i<numCourses;i++){
-            for(auto it:adj[i]){
-                ind[it]++;
-            }
+    bool canFinish(int V, vector<vector<int>>& ed) {
+        vector<int> arr(V,0);
+        vector<int> ans;
+        vector<vector<int>> adj(V);
+        for(auto it:ed){
+            adj[it[0]].push_back(it[1]);
+            arr[it[1]]++;
         }
         queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(ind[i]==0){
-                q.push(i);
-            }
+        for(int i=0;i<V;i++){
+            if(arr[i]==0) q.push(i);
         }
         while(!q.empty()){
             int node = q.front();
-            q.pop();
             ans.push_back(node);
+            q.pop();
             for(auto it:adj[node]){
-                ind[it]--;
-                if(ind[it]==0){
+                arr[it]--;
+                if(arr[it]==0){
                     q.push(it);
                 }
             }
         }
-        if(ans.size()==numCourses){
-            return true;
-        }
+        if(ans.size()==V) return true;
         return false;
     }
 };
